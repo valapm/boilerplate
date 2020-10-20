9966eb1c17c66d1e002dc7bb4a549332416f8493
Merlin Buczek <merlin.buczek@pm.me>
2020-10-20 11:41:45 +0200

    Add fixMath WIP

---
+++ tests/js/fixMath.scrypttest.js
@@ -0,0 +1,20 @@
+const { expect } = require("chai")
+const { buildContractClass } = require("scryptlib")
+
+const { compileContract } = require("../../helper")
+
+describe("Test sCrypt contract fixMath In Javascript", () => {
+  let result, fixMath
+
+  before(() => {
+    const FixMath = buildContractClass(compileContract("fixMath.scrypt"))
+    fixMath = new FixMath()
+  })
+
+  it("should return true", () => {
+    result = fixMath.testLog2(1).verify()
+    expect(result.success, result.error).to.be.true
+    result = fixMath.testLog10(1).verify()
+    expect(result.success, result.error).to.be.true
+  })
+})
